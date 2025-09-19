f5edb2eaabc557e6a7720fdc55fb00a8a828d47a
vanshikavg0816170-eng <vanshikavg0816170@gmail.com>
2025-09-19 19:46:24 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -89,7 +89,7 @@ const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
+ module.exports = { app, io };
 
-// module.exports = { app, io };
 
 
