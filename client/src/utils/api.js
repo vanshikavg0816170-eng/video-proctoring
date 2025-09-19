6d80c4767094c79f53cbde970cbe4c39775bafd6
vanshikavg0816170-eng <vanshikavg0816170@gmail.com>
2025-09-19 19:20:04 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = `${process.env.REACT_APP_BACKEND_URL}/api`;
 
 const api = axios.create({
   baseURL: API_BASE_URL,
@@ -54,3 +54,4 @@ export const uploadVideo = async (videoBlob, interviewId) => {
 };
 
 export default api;
+
